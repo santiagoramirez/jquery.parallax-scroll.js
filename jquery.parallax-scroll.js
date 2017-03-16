6371864c11a0b908da140e5dabfa1ddfccd537ff
Santiago Ramirez <hello@santiagoramirez.net>
2017-03-16 16:13:13 -0400

    First commit

---
+++ jquery.parallax-scroll.js
@@ -0,0 +1,67 @@
+/**
+ * parallaxScroll v0.1.0
+ * Santiago Ramirez
+ */
+jQuery.fn.parallaxScroll = function(options) {
+
+    var defaults = {
+        startPos : 0,
+        endPos : 0,
+        moveX : 0,
+        moveY : 0,
+        offsetX : 0,
+        offsetY : 0,
+        selector : "",
+    };
+
+    var element = this;
+
+    var init = function() {
+
+        jQuery(element).data('parallaxScroll', this);
+
+        for (option in defaults) {
+            if (typeof options[option] === 'undefined') {
+                options[option] = defaults[option];
+            }
+        }
+
+        jQuery(options.selector).css("left", "");
+
+        reactX(jQuery(this));
+        reactY(jQuery(this));
+
+        jQuery(this).scroll(function() {
+            reactX(jQuery(this));
+            reactY(jQuery(this));
+        });
+    }.bind(this)
+
+    var reactX = function(_this) {
+        var scrollPos = _this.scrollLeft();
+        if (scrollPos >= options.startPos && scrollPos <= options.endPos) {
+            var length =  (options.endPos - options.startPos);
+            var progress = (scrollPos - options.startPos) / length;
+            var pos = Math.round(progress * (options.moveX * 100));
+            jQuery(options.selector).css("left", (pos + options.offsetX) + "px");
+        }
+    }
+
+    var reactY = function(_this) {
+        var scrollPos = _this.scrollTop();
+        if (scrollPos >= options.startPos && scrollPos <= options.endPos) {
+            var length =  (options.endPos - options.startPos);
+            var progress = (scrollPos - options.startPos) / length;
+            var pos = Math.round(progress * (options.moveY * 100));
+            jQuery(options.selector).css("top", (pos + options.offsetY) + "px");
+        }
+    }
+
+    element.reload = function(options) {
+        init();
+    }
+
+    init();
+
+    return this;
+}
